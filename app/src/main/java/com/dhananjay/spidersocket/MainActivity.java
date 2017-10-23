119fa30e19e99c0ab5db3a88e63c1cd45029dd54
dj <dhananjaypnm@gmail.com>
2017-10-23 20:19:57 +0530

    add accel functionality

--- app/src/main/java/com/dhananjay/spidersocket/MainActivity.java
+++ app/src/main/java/com/dhananjay/spidersocket/MainActivity.java
@@ -1,13 +1,61 @@
 package com.dhananjay.spidersocket;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private float yValue;
+
+    private TextView accelData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        initAccelerometer();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this);
+    }
+
+    private void initViews() {
+        accelData = (TextView) findViewById(R.id.accel);
+    }
+
+    private void initAccelerometer() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        yValue = event.values[1]; // [x, y, z] are available values
+        accelData.setText(String.valueOf(yValue));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }

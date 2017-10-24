817d61f5b443c7a2377d8c2b1ffae32299f37193
dj <dhananjaypnm@gmail.com>
2017-10-25 00:19:01 +0530

    add comments

--- app/src/main/java/com/dhananjay/spidersocket/EditIpActivity.java
+++ app/src/main/java/com/dhananjay/spidersocket/EditIpActivity.java
@@ -10,7 +10,9 @@ import android.widget.Toast;
 
 public class EditIpActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String PREF_KEY_IP_ADDRESS = "PREF_KEY_IP_ADDRESS";
+    private static final String TAG = "MainActivity"; // TAG to identify log messages
+
+    private static final String PREF_KEY_IP_ADDRESS = "PREF_KEY_IP_ADDRESS"; // key to store the IP address
 
     private EditText editIp;
     private Button confirm;
@@ -18,25 +20,26 @@ public class EditIpActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_ip);
-        initViews();
+        setContentView(R.layout.activity_edit_ip); // specify the layout to be inflated
+        initViews(); // initialize views
     }
 
     private void initViews() {
         editIp = (EditText) findViewById(R.id.enter_ip);
         confirm = (Button) findViewById(R.id.confirm);
-        confirm.setOnClickListener(this);
+        confirm.setOnClickListener(this); // handle button click
     }
 
     @Override
     public void onClick(View v) {
+        // if the input is not empty, store the IP address
         if (!editIp.getText().toString().equals("")){
             PreferenceManager.getDefaultSharedPreferences(getApplication()).edit()
                     .putString(PREF_KEY_IP_ADDRESS, editIp.getText().toString())
                     .commit();
             finish();
         }else{
-            Toast.makeText( getApplicationContext(), "PLEASE ENTER VALID IP", Toast.LENGTH_SHORT).show();
+            Toast.makeText( getApplicationContext(), "PLEASE ENTER VALID IP", Toast.LENGTH_SHORT).show(); // display error message
         }
 
     }
--- app/src/main/java/com/dhananjay/spidersocket/MainActivity.java
+++ app/src/main/java/com/dhananjay/spidersocket/MainActivity.java
@@ -22,9 +22,11 @@ import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
 
-    private static final String TAG = "MainActivity";
-    private static final String PREF_KEY_IP_ADDRESS = "PREF_KEY_IP_ADDRESS";
+    private static final String TAG = "MainActivity"; // TAG to identify log messages
 
+    private static final String PREF_KEY_IP_ADDRESS = "PREF_KEY_IP_ADDRESS"; // key to fetch previously stored IP address
+
+    // sensors
     private SensorManager sensorManager;
     private Sensor accelerometer;
 
@@ -36,25 +38,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView accelData;
     private ImageButton editIp;
 
+    // the entry point of the program
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // call the super class
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // specifies which layout file to inflate
 
-        ipAddress = PreferenceManager.getDefaultSharedPreferences(getApplication()).getString(PREF_KEY_IP_ADDRESS, null);
+        ipAddress = PreferenceManager.getDefaultSharedPreferences(getApplication()).getString(PREF_KEY_IP_ADDRESS, null); // check if a valid IP address is already saved
 
-        initViews();
+        initViews(); // initialize the views
 
-        initAccelerometer();
+        initAccelerometer(); // initialize the sensor
 
-        initSocket();
+        initSocket(); // initialize the socket
     }
 
+    // method executed when the activity resumes
     @Override
     protected void onResume() {
         super.onResume();
 
+        // make the activity fullscreen
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -63,53 +68,58 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME); // start listening to sensor events
     }
 
+    // last method called before the activity is destroyed
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(this); // stop receiving sensor updates
 
+        // if socket is not null, disconnect it
         if(socket != null){
             socket.disconnect();
         }
     }
 
+    // initialize views
     private void initViews() {
         accelData = (TextView) findViewById(R.id.accel_value);
         editIp = (ImageButton) findViewById(R.id.edit_ip);
-        editIp.setOnClickListener(this);
+        editIp.setOnClickListener(this); // handle button click
     }
 
+    // initialize accel
     private void initAccelerometer() {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
+    // initalize socket
     private void initSocket() {
-        Log.d(TAG, "initSocket: "+ipAddress);
+        // check if the IP address is valid, is so create and connect to the socket
         if( ipAddress != null ){
             try {
-                socket = IO.socket("http://0.0.0.0:8080");
+                socket = IO.socket("http://0.0.0.0:8080"); // ip address : port no
                 socket.connect();
             } catch (URISyntaxException e) {
-                Log.e(TAG, "initSocket: ", e);
+                Log.e(TAG, "initSocket: ", e); // log errors if any
             }
         }else{
-            Toast.makeText( getApplicationContext(), "ENTER IP", Toast.LENGTH_SHORT).show();
+            Toast.makeText( getApplicationContext(), "ENTER IP", Toast.LENGTH_SHORT).show(); // display message to user that IP is invalid
         }
     }
 
+    // called every time sensor data is changed
     @Override
     public void onSensorChanged(SensorEvent event) {
         yValue = event.values[1]; // [x, y, z] are available values
-        yValue = Math.round(yValue * 1000.0) / 1000.0;
-        accelData.setText(String.valueOf(yValue));
-        //Log.d(TAG, "onSensorChanged: "+ yValue);
+        yValue = Math.round(yValue * 1000.0) / 1000.0; // round off up to thousandth of decimal
+        accelData.setText(String.valueOf(yValue)); // update the accel data
         if(socket!=null) {
-            socket.emit("yvalue", yValue);
+            socket.emit("yvalue", yValue); // send the values to server
         }
     }
 
@@ -117,10 +127,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
+    // handle image button click
     @Override
     public void onClick(View v) {
+        // create an intent to navigate to EditIpActivity
         Intent editIpIntent = new Intent(this, EditIpActivity.class);
         startActivity(editIpIntent);
-        finish();
+        finish(); // destroy this activity
     }
 }
